e3abe882659790c35816c07d2d74f91ba8a4946f
Jonatan Kaplan <jonatankaplan@gmail.com>
2018-03-09 15:43:59 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,11 @@
+var chocolateBars = [
+ 'snickers',
+ 'hundred grand',
+ 'kitkat',
+ 'skittles'
+]
+
+function addElementToBeginningOfArray(array, element) {
+   var new_array = [element, array[0]]
+   return new_array
+}
